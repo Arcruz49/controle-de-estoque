1943669fda630fc6b684fa67798459437a8ef962
Henrique Elias <rike.oliveira.2012@gmai.com>
2024-06-16 14:59:13 -0300

    Agora é preciso estar logado em todos os .php

--- php/diminuiEstoque.php
+++ php/diminuiEstoque.php
@@ -17,16 +17,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $cdproduto = mysqli_real_escape_string($conexao, $_POST['cdProduto']);
     
-    $sqlValidacao = "SELECT quantidade FROM tb_produtos WHERE cdProduto = '$cdproduto'";
-    $resultValidacao = mysqli_query($conexao, $sqlValidacao);
-
-    $row = mysqli_fetch_assoc($resultValidacao);
-
-    if ($row['quantidade'] <= 0) {
-        echo "O estoque já está vazio";
-        exit;
-    }
-
     $sql = "UPDATE tb_produtos SET quantidade = quantidade - 1 WHERE cdProduto = '$cdproduto';";
     
     $result = mysqli_query($conexao, $sql);
