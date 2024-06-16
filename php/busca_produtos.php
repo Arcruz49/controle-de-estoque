b4a1a509180a8ef35780a3a308cf7ae0e6d688bd
Arthur Cruz <arthur.cruz@objetivasolucao.com.br>
2024-06-16 01:17:45 -0300

    datatable adicionada e comunicando com a listagem no backend

---
+++ php/busca_produtos.php
@@ -0,0 +1,23 @@
+<?php
+header('Content-Type: application/json');
+
+include "../config/dbconfig.php";
+
+$searchTerm = isset($_GET['search']) ? mysqli_real_escape_string($conexao, $_GET['search']) : '';
+
+$query = "SELECT cdProduto, nmProduto, descProduto, preco, quantidade FROM tb_produtos WHERE nmProduto like '%$searchTerm%'";
+$result = mysqli_query($conexao, $query);
+
+$data = array();
+while ($row = mysqli_fetch_assoc($result)) {
+    $data[] = $row;
+}
+
+$response = array(
+    "data" => $data
+);
+
+echo json_encode($response);
+
+mysqli_close($conexao);
+?>
