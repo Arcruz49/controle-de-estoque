e550e0bfb5518e31239634f00f2b4ed2fbdc283b
Arthur Cruz <arthur.cruz@objetivasolucao.com.br>
2024-06-16 02:13:44 -0300

    HAAHAHAHAHAHAHAHAHHAHAHAHAHAAHAHAHAHAH função de deletar produto

--- php/excluir_produtos.php
+++ php/excluir_produtos.php
@@ -5,24 +5,23 @@ session_start();
 include "../config/dbconfig.php";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // TODO: Mudar para o que vai ser usado no html
     if (empty($_POST["cdProduto"])) {
-        echo "Código do produto não foi achado.";
+        echo "Código do produto não foi encontrado.";
         exit;
     }
 
-    $cdproduto = mysqli_real_escape_string($conexao, $_POST['cdproduto']);
+    $cdproduto = mysqli_real_escape_string($conexao, $_POST['cdProduto']);
 
     $delete = "DELETE FROM tb_produtos WHERE cdProduto = '$cdproduto';";
     $result = mysqli_query($conexao, $delete);
-    if ($result == false) {
+
+    if ($result) {
+        echo "success";
+        exit;
+    } else {
         echo "Não foi possível deletar o produto.";
         exit;
     }
-
-    echo "success";
-    exit;
 }
 
 ?>
-
