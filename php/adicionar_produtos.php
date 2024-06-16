9416f568b46be14b48c3f6f4652378e2ecfb2eff
Henrique Elias <rike.oliveira.2012@gmai.com>
2024-06-15 23:30:13 -0300

    adicionar está feito

--- php/adicionar_produtos.php
+++ php/adicionar_produtos.php
@@ -4,21 +4,42 @@ session_start();
 
 include "../config/dbconfig.php";
 
+
+/*
+    cdProduto,
+    nmProduto,
+    descProduto,
+    preco,
+    quantidade,
+    dtCriacao,
+ */
+
+    
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if (empty($_POST["usernameEmail"]) || empty($_POST["passwordLogin"])) {
+    // Mudar para o que vai ser usado no html
+    if (empty($_POST["cdProduto"]) || empty($_POST["nmProduto"]) || empty($_POST["descProduto"]) || empty($_POST["preco"]) || empty($_POST["quantidade"])) {
         exit;
     }
 
-    $current_date = date("Y/m/d");
-    $insert = "INSERT INTO cadUsuario (nmUsuario, senha, dtCriacao) VALUES (cruz, admin, $current_date);";
-
-
-    $result = mysqli_query($conexao, $sql);
-    while($fetch = mysqli_fetch_row($result)) {
-        echo "<p>". $fetch[0] . " - " . $fetch[1] . " - " . "</p>";
-        
-
+    $cdproduto = mysqli_real_escape_string($conexao, $_POST['cdproduto']);
+    $nmproduto = mysqli_real_escape_string($conexao, $_POST['nmproduto']);
+    $descproduto = mysqli_real_escape_string($conexao, $_POST['descproduto']);
+    if (strlen($descproduto) >= 100) {
+        $descproduto = substr($descproduto, 0, 100);
     }
+
+    $preco = mysqli_real_escape_string($conexao, $_POST['preco']);
+    $quantidade = mysqli_real_escape_string($conexao, $_POST['quantidade']);
+
+    $insert = "INSERT INTO tb_produtos (nmProduto, descProduto, preco, quantidade, dtCriacao) VALUES ('$nmproduto', '$descproduto', '$preco', '$quantidade' NOW());";
+    $result = mysqli_query($conexao, $insert);
+    if ($result == false) {
+        echo "Não foi possível inserir os dados no banco.";
+        exit;
+    }
+
+    echo "success";
+    exit;
 }
 
 mysqli_close($conexao);
