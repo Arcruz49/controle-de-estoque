e98dcff7650feba1118e4040c50c45addefecc29
Arthur Cruz <arthur.cruz@objetivasolucao.com.br>
2024-06-16 21:47:01 -0300

    adicionar produto agora funciona HAHSAHAHA

--- php/adicionar_produtos.php
+++ php/adicionar_produtos.php
@@ -8,36 +8,26 @@ if (!isset($_SESSION['nome'])) {
 }
 
 include "../config/dbconfig.php";
-
-/*
-    cdProduto,
-    nmProduto,
-    descProduto,
-    preco,
-    quantidade,
-    dtCriacao,
- */
-
     
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // TODO: Mudar para o que vai ser usado no html
-    if (empty($_POST["cdProduto"]) || empty($_POST["nmProduto"]) || empty($_POST["descProduto"]) || empty($_POST["preco"]) || empty($_POST["quantidade"])) {
-        echo "Coisas obrigatórias.";
+    if (empty($_POST["nmProduto"]) || empty($_POST["descProduto"]) || empty($_POST["preco"]) || empty($_POST["quantidade"])) {
+        echo "Campos obrigatórias.";
         exit;
     }
-
-    $cdproduto = mysqli_real_escape_string($conexao, $_POST['cdproduto']);
-    $nmproduto = mysqli_real_escape_string($conexao, $_POST['nmproduto']);
-    $descproduto = mysqli_real_escape_string($conexao, $_POST['descproduto']);
-    if (strlen($descproduto) >= 100) {
-        $descproduto = substr($descproduto, 0, 100);
+    $nmproduto = mysqli_real_escape_string($conexao, $_POST['nmProduto']);
+    $descproduto = mysqli_real_escape_string($conexao, $_POST['descProduto']);
+    if (strlen($descproduto) >= 200) {
+        $descproduto = substr($descproduto, 0, 200);
     }
 
     $preco = mysqli_real_escape_string($conexao, $_POST['preco']);
     $quantidade = mysqli_real_escape_string($conexao, $_POST['quantidade']);
 
-    $insert = "INSERT INTO tb_produtos (nmProduto, descProduto, preco, quantidade, dtCriacao) VALUES ('$nmproduto', '$descproduto', '$preco', '$quantidade' NOW());";
+    $insert = "INSERT INTO tb_produtos (nmProduto, descProduto, preco, quantidade, dtCriacao) 
+               VALUES ('$nmproduto', '$descproduto', '$preco', '$quantidade', NOW())";    
+    
     $result = mysqli_query($conexao, $insert);
+
     if ($result == false) {
         echo "Não foi possível inserir os dados no banco.";
         exit;
