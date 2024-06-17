e198a104fb95ef20cdeebb62a459ea2531f4d291
hen-rikes <162860433+hen-rikes@users.noreply.github.com>
2024-06-17 20:21:25 -0300

    Adiciona busca usuario .php

---
+++ php/busca_usuarios.php
@@ -0,0 +1,23 @@
+<?php
+header('Content-Type: application/json');
+
+include "../config/dbconfig.php";
+
+$searchTerm = isset($_GET['search']) ? mysqli_real_escape_string($conexao, $_GET['search']) : '';
+
+$query = "SELECT cdUsuario, nmUsuario, email, senha FROM cadUsuario WHERE nmUsuario like '%$searchTerm%'";
+$result = mysqli_query($conexao, $query);
+
+$data = array();
+while ($row = mysqli_fetch_assoc($result)) {
+    $data[] = $row;
+}
+
+$response = array(
+    "data" => $data
+);
+
+echo json_encode($response);
+
+mysqli_close($conexao);
+?>
